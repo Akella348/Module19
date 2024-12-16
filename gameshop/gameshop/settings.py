c9e68761a498632059afbcea9f53ef51249d3a6a
Akella348 <akella348@gmail.com>
2024-12-16 11:13:44 +0400

    5 iter

--- gameshop/gameshop/settings.py
+++ gameshop/gameshop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+# import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'gameshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gameshop', # Имя базы данных
+        'USER': 'postgres', # Пользователь PostgreSQL
+        'PASSWORD': '123123123', # Пароль PostgreSQL
+        'HOST': 'localhost', # Адрес сервера базы данных
+        'PORT': '5433', # Порт подключения
+
     }
 }
 
